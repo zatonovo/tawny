23ab6c31b2ca3f907f25f27650c9c996e0c1133d
Brian Rowe <rowe@muxspace.com>
2011-10-20 21:39:06 +0000

    Moved some RMT-specific functions over to futile.matrix

--- R/random_matrix.R
+++ R/random_matrix.R
@@ -171,62 +171,6 @@ mp.density.kernel.correlation <- function(h, adjust=0.2, kernel='e', ...)
   return(ds)
 }
 
-# Calculate and plot the theoretical density distribution
-# e.values - The eigenvalues to plot the density against. This can really be any
-#   point on the xaxis.
-mp.theory <- function(Q, sigma, e.values=NULL, steps=200)
-{
-  # Plot a range of values
-  if (is.null(e.values)) { e.values <- mp.lambdas(Q,sigma,steps) }
-  rho <- mp.rho(Q,sigma, e.values)
-
-  if (anylength(e.values) > 1)
-  {
-    l.min <- mp.eigen.min(Q,sigma)
-    l.max <- mp.eigen.max(Q,sigma)
-    xs <- seq(round(l.min-1), round(l.max+1), (l.max-l.min)/steps)
-    main <- paste('Marcenko-Pastur Distribution for Q',Q,'and sigma',sigma)
-    plot(xs,rho, xlim=c(0,6), type='l', main=main)
-  }
-  rho
-}
-
-
-# Generate eigenvalues for theoretical Marcenko-Pastur distribution
-mp.lambdas <- function(Q,sigma, steps)
-{
-  l.min <- mp.eigen.min(Q,sigma)
-  l.max <- mp.eigen.max(Q,sigma)
-
-  logger(DEBUG, sprintf("min eigenvalue:%s",l.min))
-  logger(DEBUG, sprintf("max eigenvalue:%s",l.max))
-
-  evs <- seq(round(l.min-1), round(l.max+1), (l.max-l.min)/steps)
-  evs[evs < l.min] <- l.min
-  evs[evs > l.max] <- l.max
-
-  evs
-}
-
-# This provides the theoretical density for a set of eigenvalues. These are
-# really just points along the x axis for which the eigenvalue density is
-# desired.
-# e.values can be a vector of eigen values or a single eigen value
-mp.rho <- function(Q,sigma, e.values)
-{
-  l.min <- mp.eigen.min(Q,sigma)
-  l.max <- mp.eigen.max(Q,sigma)
-
-  k <- Q / (2*pi*sigma^2)
-  rho <- k * sqrt(pmax(0, (l.max-e.values)*(e.values-l.min)) ) / e.values
-  rho
-}
-
-# Get maximum eigenvalue specified by Marcenko-Pastur
-mp.eigen.max <- function(Q,sigma) { sigma^2 * (1 + sqrt(1/Q))^2 }
-
-# Get minimum eigenvalue specified by Marcenko-Pastur
-mp.eigen.min <- function(Q,sigma) { sigma^2 * (1 - sqrt(1/Q))^2 }
 
 # Fit the appropriate MP curve to the data. This will estimate Q and sigma.
 # hist is a histogram object
